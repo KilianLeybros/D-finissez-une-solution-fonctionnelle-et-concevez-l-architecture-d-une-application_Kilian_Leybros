3f4b780193a228d47a93dc10406a301e80abe809
Kilian <k.leybros.15@gmail.com>
2025-01-16 19:53:11 +0100

    back initialisation

---
+++ ycyw-back/src/main/java/com/openclassrooms/ycyw_back/YcywBackApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.ycyw_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcywBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YcywBackApplication.class, args);
+	}
+
+}
