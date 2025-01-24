87140f1de969c8728a0bb27a06a81bae49bf34b2
Kilian <k.leybros.15@gmail.com>
2025-01-24 16:00:27 +0100

    websockets

--- ycyw-front/src/app/views/request/request.component.ts
+++ ycyw-front/src/app/views/request/request.component.ts
@@ -1,15 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { RequestListComponent } from './components/request-list.component';
 import { ChatComponent } from '../../shared/components/chat.component';
-import { CustomerRequest } from 'app/shared/interfaces/request.interface';
+
 import { RouterOutlet } from '@angular/router';
+import { SupportRequest } from 'app/core/interfaces';
+import { SupportRequestService } from 'app/core/services/support-request.service';
 
 @Component({
   selector: 'app-request',
   imports: [RequestListComponent, RouterOutlet],
   template: `
     <app-request-list
-      [requests]="requests()"
+      [requests]="requestsList()"
       class="request-max-h w-half xs-w-full card"
     />
     <div class="request-max-h w-half xs-w-full flex">
@@ -18,6 +20,7 @@ import { RouterOutlet } from '@angular/router';
     </div>
   `,
   styles: `:host {
+    justify-content: center;
     flex: 1 1 auto;
     display: flex;
     gap:24px;
@@ -26,20 +29,14 @@ import { RouterOutlet } from '@angular/router';
       flex-direction: column;
     }
   }
-  
+
   .request-max-h{
-    max-height: 802px; 
+    max-height: 802px;
   }`,
 })
 export class RequestComponent {
-  requests = signal<CustomerRequest[]>([
-    {
-      id: '1',
-      customerName: 'Kilian',
-    },
-    {
-      id: '2',
-      customerName: 'Jean',
-    },
-  ]);
+  readonly supportRequestService = inject(SupportRequestService);
+  requestsList = computed(
+    () => this.supportRequestService.supportRequestsResource.value() || []
+  );
 }
